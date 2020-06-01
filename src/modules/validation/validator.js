4c24f9f7c0d12479a29ca0df70d08cc7febcb55d
Levysantiago <levyssantiago@gmail.com>
2020-06-01 15:44:00 -0300

    creating the assignmentModel error tests

--- src/modules/validation/validator.js
+++ src/modules/validation/validator.js
@@ -1,14 +1,30 @@
 function createValidator() {
+  const messages = {
+    /**
+     * @param {string} arg
+     */
+    required: (arg) => {
+      return "The '" + arg + "' argument is required.";
+    },
+    /**
+     * @param {string} arg
+     * @param {string} type
+     */
+    wrongType: (arg, type) => {
+      return "The '" + arg + "' argument must be of type " + type;
+    },
+  };
+
   function _throwError(message) {
     throw new Error(message);
   }
 
   function _throwRequired(arg) {
-    _throwError("The '" + arg + "' argument is required.");
+    _throwError(messages.required(arg));
   }
 
   function _throwWrongType(arg, type) {
-    _throwError("The '" + arg + "' argument must be of type " + type);
+    _throwError(messages.wrongType(arg, type));
   }
 
   function validate(list) {
@@ -25,7 +41,7 @@ function createValidator() {
     }
   }
 
-  return { validate };
+  return { validate, messages };
 }
 
 module.exports = createValidator;
--- test/models/assignment.spec.js
+++ test/models/assignment.spec.js
@@ -1,12 +1,20 @@
 const createAssignmentModel = require("../../src/modules/models/assignment");
 const helpers = require("../../src/utils/helpers");
 const assert = require("assert");
+const createValidator = require("../../src/modules/validation/validator");
 
+/**
+ * Testing Assignment Model
+ */
 describe("Assignment Model", () => {
   const statement = "assignment";
   const variable = "val";
   const value = "1";
   const assignmentModel = createAssignmentModel(variable, value);
+
+  /**
+   * If object was sussessfuly created
+   */
   it("Asserting object", () => {
     // Asserting if is an object
     assert.ok(helpers.isObject(assignmentModel), "Model is not an Object.");
@@ -14,6 +22,9 @@ describe("Assignment Model", () => {
     assert.ok(!helpers.isObjEmpty(assignmentModel), "Object is empty.");
   });
 
+  /**
+   * Initial properties
+   */
   it("Asserting initial properties", () => {
     // Statement property
     assert.equal(
@@ -35,8 +46,57 @@ describe("Assignment Model", () => {
     );
   });
 
-  //   it("Testing argument complete fault", () => {
-  //     let assignmentModel = createAssignmentModel(1, 2);
-  //     console.log(assignmentModel);
-  //   });
+  // Getting messages from validator for error testing
+  const { messages } = createValidator();
+
+  /**
+   * Testing type Errors
+   */
+  it("Testing argument type error", () => {
+    // Setting up error messages
+    const message_1 = messages.wrongType("_variable", "string");
+    const message_2 = messages.wrongType("_value", "string");
+    const message_3 = message_1;
+
+    // Asserting errors
+    assert.throws(
+      () => createAssignmentModel(1, "2"),
+      Error(message_1),
+      "Error should thrown [" + message_1 + "] (line 1)"
+    );
+
+    assert.throws(
+      () => createAssignmentModel("val", 2),
+      Error(message_2),
+      "Error should thrown [" + message_2 + "] (line 2)"
+    );
+
+    assert.throws(
+      () => createAssignmentModel(1, 2),
+      Error(message_3),
+      "Error should thrown [" + message_3 + "] (line 3)"
+    );
+  });
+
+  /**
+   * Testing required Errors
+   */
+  it("Testing argument required error", () => {
+    // Setting up error messages
+    const message_1 = messages.required("_variable");
+    const message_2 = messages.required("_value");
+
+    // Asserting errors
+    assert.throws(
+      () => createAssignmentModel(),
+      Error(message_1),
+      "Error should thrown [" + message_1 + "] (line 1)"
+    );
+
+    assert.throws(
+      () => createAssignmentModel("val"),
+      Error(message_2),
+      "Error should thrown [" + message_2 + "] (line 2)"
+    );
+  });
 });
