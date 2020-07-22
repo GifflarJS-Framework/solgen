b8ebd4d4d4fbefa98b39e872eb8957a3f22b3d30
Levysantiago <levyssantiago@gmail.com>
2020-07-22 18:17:44 -0300

    removing parser

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,10 @@
 require("module-alias/register");
 const createContractManager = require("@management/contractManager");
 const createContract = require("@management/contract");
-const createJsonParser = require("@iot/jsonParser");
 
 const Gifflar = {
   createContractManager,
   createContract,
-  createJsonParser,
 };
 
 module.exports = Gifflar;
