e0c9d9894adeae10d9f9277e733db58f7757cce3
Levy Santiago <levyssantiago@gmail.com>
2022-08-09 10:14:26 -0300

    [#5] refactor: renaming modifier DTO

--- src/modules/models/modifier/types/ICreateModifierDTO.ts
+++ src/modules/models/modifier/types/ICreateModifierDTO.ts
@@ -1,7 +1,7 @@
 import { IInput } from "@models/function/types/IInput";
 import { IVariable } from "@models/variable/types/IVariable";
 
-export interface ICreateModifierModelDTO {
+export interface ICreateModifierDTO {
   title: string;
   args: Array<IInput>;
   globalVars?: Array<IVariable>;
