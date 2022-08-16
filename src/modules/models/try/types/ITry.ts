ef5eb8aa607638c37b1751f683a4b7dfb49664a1
Levy Santiago <levyssantiago@gmail.com>
2022-08-16 10:00:49 -0300

    [#17] Adding comment to ITry

--- src/modules/models/try/types/ITry.ts
+++ src/modules/models/try/types/ITry.ts
@@ -5,6 +5,7 @@ import { INewContract } from "@models/newcontract/types/INewContract";
 
 export interface ITry extends IStackItem {
   statement: "try";
+  /** Try can only be used with external function calls and contract creation calls */
   expression: INewContract | IMethodCall;
   parameters: Array<IInput>;
 }
