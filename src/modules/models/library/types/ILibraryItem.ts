bb9865be394a04a820415192aba593c1f14e1424
Levy Santiago <levyssantiago@gmail.com>
2022-08-24 11:21:06 -0300

    fix: including 'using' type to library and contract models

--- src/modules/models/library/types/ILibraryItem.ts
+++ src/modules/models/library/types/ILibraryItem.ts
@@ -4,9 +4,11 @@ import { IFunction } from "@models/function/types/IFunction";
 import { IModifier } from "@models/modifier/types/IModifier";
 import { IStateMapping } from "@models/stateMapping/types/IStateMapping";
 import { IStateVariable } from "@models/stateVariable/types/IStateVariable";
+import { IUsing } from "@models/using/types/IUsing";
 
 export interface ILibraryItem {
   name: string;
+  usings: Array<IUsing>;
   variables: Array<IStateVariable>;
   mappings: Array<IStateMapping>;
   events: Array<IEvent>;
