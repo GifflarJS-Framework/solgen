a295b81d375e646453d92a02e250595e86f16c22
Levy Santiago <levyssantiago@gmail.com>
2022-08-30 15:48:53 -0300

    refactor: rebuilding

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createContract = exports.createContractManager = void 0;
+exports.createGifflarManager = exports.createGifflarInterface = exports.createGifflarLibrary = exports.createGifflarContract = void 0;
 require("reflect-metadata");
 require("./modules");
 var tsyringe_1 = require("tsyringe");
-var createContract = function (name) {
+var createGifflarContract = function (name) {
     var gifflarContractModel = tsyringe_1.container.resolve("GifflarContractModel");
     return gifflarContractModel.execute(name);
 };
-exports.createContract = createContract;
-var createContractManager = function () {
+exports.createGifflarContract = createGifflarContract;
+var createGifflarLibrary = function (name) {
+    var gifflarContractModel = tsyringe_1.container.resolve("GifflarLibraryModel");
+    return gifflarContractModel.execute(name);
+};
+exports.createGifflarLibrary = createGifflarLibrary;
+var createGifflarInterface = function (name) {
+    var gifflarContractModel = tsyringe_1.container.resolve("GifflarInterfaceModel");
+    return gifflarContractModel.execute(name);
+};
+exports.createGifflarInterface = createGifflarInterface;
+var createGifflarManager = function () {
     return tsyringe_1.container.resolve("GifflarManager");
 };
-exports.createContractManager = createContractManager;
+exports.createGifflarManager = createGifflarManager;
