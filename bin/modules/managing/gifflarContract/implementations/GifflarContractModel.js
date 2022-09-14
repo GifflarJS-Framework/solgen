ed1e2d1ad85b42f5ed3e49aa08edb6c41ee9708f
Levy Santiago <levyssantiago@gmail.com>
2022-09-14 17:36:02 -0300

    refactor: rebuilding

--- bin/modules/managing/gifflarContract/implementations/GifflarContractModel.js
+++ bin/modules/managing/gifflarContract/implementations/GifflarContractModel.js
@@ -105,8 +105,7 @@ var GifflarContractModel = /** @class */ (function () {
                     callback(errors);
                 }
                 // Inserting contract name in compiled json
-                gContract.json.contracts.jsons[gContract.getName()].contractName =
-                    gContract.getName();
+                gContract.json.contracts.jsons[gContract.getName()] = __assign({ contractName: gContract.getName() }, gContract.json.contracts.jsons[gContract.getName()]);
                 // Inserting contract networks in compiled json
                 gContract.json.contracts.jsons[gContract.getName()]["networks"] = {};
                 return gContract.json;
--- bin/modules/managing/gifflarManager/implementations/GifflarManager.js
+++ bin/modules/managing/gifflarManager/implementations/GifflarManager.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -189,9 +200,7 @@ var GifflarManager = /** @class */ (function () {
                 callback(json.errors);
             }
             // Inserting contract name in compiled json
-            component.json.contracts.jsons[component.getName()] = {
-                contractName: component.getName(),
-            };
+            component.json.contracts.jsons[component.getName()] = __assign({ contractName: component.getName() }, component.json.contracts.jsons[component.getName()]);
             // Inserting contract networks in compiled json
             component.json.contracts.jsons[component.getName()]["networks"] = {};
             return json;
