db904220f8f461eda442feaa0ec5eb72a4c2c719
Levy Santiago <levyssantiago@gmail.com>
2022-09-14 17:31:41 -0300

    refactor: rebuilding

--- bin/modules/managing/gifflarContract/implementations/GifflarContractModel.js
+++ bin/modules/managing/gifflarContract/implementations/GifflarContractModel.js
@@ -105,9 +105,8 @@ var GifflarContractModel = /** @class */ (function () {
                     callback(errors);
                 }
                 // Inserting contract name in compiled json
-                gContract.json.contracts.jsons[gContract.getName()] = {
-                    contractName: gContract.getName(),
-                };
+                gContract.json.contracts.jsons[gContract.getName()].contractName =
+                    gContract.getName();
                 // Inserting contract networks in compiled json
                 gContract.json.contracts.jsons[gContract.getName()]["networks"] = {};
                 return gContract.json;
