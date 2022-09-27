93226875cc4f2abc80bfbc7e309d751eab2fc500
Levy Santiago <levyssantiago@gmail.com>
2022-09-27 15:47:19 -0300

    docs: factories functions

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,15 @@ import { IGifflarLibraryModel } from "@managing/gifflarLibrary/types/IGifflarLib
 import { IGifflarInterface } from "@managing/GifflarInterface/types/IGifflarInterface";
 import { IGifflarInterfaceModel } from "@managing/GifflarInterface/types/IGifflarInterfaceModel";
 
+/**
+ * Creates a new Gifflar Contract
+ * @param name The name of the smart contract.
+ * @returns A new Gifflar Contract
+ * @example
+ * import { createGifflarContract } from "gifflar-library";
+ *
+ * const gContract = createGifflarContract("MyContract");
+ */
 const createGifflarContract = (name: string): IGifflarContract => {
   const gifflarContractModel: IGifflarContractModel = container.resolve(
     "GifflarContractModel"
@@ -16,6 +25,15 @@ const createGifflarContract = (name: string): IGifflarContract => {
   return gifflarContractModel.execute(name);
 };
 
+/**
+ * Creates a new Gifflar Library
+ * @param name The name of the smart contract library.
+ * @returns A new Gifflar Library
+ * @example
+ * import { createGifflarLibrary } from "gifflar-library";
+ *
+ * const gLibrary = createGifflarLibrary("MyLibrary");
+ */
 const createGifflarLibrary = (name: string): IGifflarLibrary => {
   const gifflarContractModel: IGifflarLibraryModel = container.resolve(
     "GifflarLibraryModel"
@@ -23,6 +41,15 @@ const createGifflarLibrary = (name: string): IGifflarLibrary => {
   return gifflarContractModel.execute(name);
 };
 
+/**
+ * Creates a new Gifflar Interface
+ * @param name The name of the smart contract interface.
+ * @returns A new Gifflar Interface
+ * @example
+ * import { createGifflarInterface } from "gifflar-library";
+ *
+ * const gInterface = createGifflarInterface("MyInterface");
+ */
 const createGifflarInterface = (name: string): IGifflarInterface => {
   const gifflarContractModel: IGifflarInterfaceModel = container.resolve(
     "GifflarInterfaceModel"
@@ -30,6 +57,14 @@ const createGifflarInterface = (name: string): IGifflarInterface => {
   return gifflarContractModel.execute(name);
 };
 
+/**
+ * Creates a new Gifflar Manager to manage many Gifflar Components (Contracts, Libraries and Interfaces).
+ * @returns A new Gifflar Manager
+ * @example
+ * import { createGifflarManager } from "gifflar-library";
+ *
+ * const gManager = createGifflarManager();
+ */
 const createGifflarManager = (): IGifflarManager => {
   return container.resolve("GifflarManager");
 };
