728c04d1a82190eb910046be5179c367ecd2d205
Levy Santiago <levyssantiago@gmail.com>
2022-09-29 17:28:40 -0300

    refactor: fallback accepting modifiers

--- src/modules/models/definitions/fallback/types/ICreateFallbackDTO.ts
+++ src/modules/models/definitions/fallback/types/ICreateFallbackDTO.ts
@@ -1,6 +1,10 @@
 import { IVariable } from "@models/definitions/stateVariable/types/IVariable";
+import { IModifierInvocation } from "../../function/types/IModifierInvocation";
 
 export interface ICreateFallbackDTO {
   stateVars: Array<IVariable>;
   isPayable?: boolean;
+  modifiers?: Array<IModifierInvocation>;
+  overrides?: boolean;
+  virtual?: boolean;
 }
--- src/modules/writers/definitions/fallbackWriter/implementations/FallbackWriter.ts
+++ src/modules/writers/definitions/fallbackWriter/implementations/FallbackWriter.ts
@@ -1,4 +1,5 @@
 import { IFallback } from "@models/definitions/fallback/types/IFallback";
+import helpers from "@utils/helpers";
 import { IContentWriter } from "@writers/definitions/contentWriter/types/IContentWriter";
 import { inject, injectable } from "tsyringe";
 import { IFallbackWriter } from "../types/IFallbackWriter";
@@ -15,11 +16,24 @@ class FallbackWriter implements IFallbackWriter {
     let txt_payable = "";
     if (fallback.isPayable) txt_payable = " payable";
 
+    // Preparing modifiers text
+    let txt_modifiers = "";
+    if (fallback.modifiers) {
+      fallback.modifiers.map((modifier) => {
+        // Modifier name
+        txt_modifiers += ` ${modifier.name}`;
+        // Modifier args
+        if (modifier.args) {
+          txt_modifiers += `(${helpers.getCommaExpression(modifier.args)})`;
+        }
+      });
+    }
+
     // Content
     const txt_content = this.contentWriter.write(fallback.content);
 
     // Final text
-    const text = `fallback() external${txt_payable}{\n${txt_content}}\n\n`;
+    const text = `fallback() external${txt_payable}${txt_modifiers}{\n${txt_content}}\n\n`;
 
     return text;
   }
